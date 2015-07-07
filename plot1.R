42e9c7005f77ee5b767f59ed47cf702a7e90461b
biobossx99 <kimchicat@gmail.com>
2015-07-07 01:28:07 -0700

    Uploading
    Uploading to Website

---
+++ plot1.R
@@ -0,0 +1,35 @@
+#Exploratory Data Analysis Assignment 1 - Coursera
+#Plot 1
+#Allen Seol 
+
+#Packages Used - dplyr
+#install.packages("dplyr")
+#library("dplyr")
+
+#Loading Data
+power <- read.table("household_power_consumption.txt", sep = ";", header = TRUE)
+
+#Converting to TBL Dataframe (working copy)
+power2 <- tbl_df(power)
+
+#Converting Time/Date into correct classes
+power2 <- mutate(power2, Date = as.Date(power2$Date, format ="%d/%m/%Y"))
+power2 <- mutate(power2, Time = paste(power2$Date,power2$Time))
+power2 <- mutate(power2, Time = as.POSIXct(power2$Time,format = "%Y-%m-%d %H:%M:%S"))
+
+#Subsetting Into 2007-02-01 and 2007-02-02 Dates and combing into Power5
+power3 <- filter(power2, Date == as.Date("2007-02-01")) 
+power4 <- filter(power2, Date == as.Date("2007-02-02"))
+power5 <- bind_rows(power3,power4)
+
+#Converting Global_active_power from factor to integer
+power5 <- mutate(power5, Global_active_power = as.numeric(as.character(power5$Global_active_power)))
+
+#Exporting to Graphic Device
+png(file = "plot1.png", width = 480, height = 480)
+
+#Making Histogram
+hist(power5$Global_active_power, col = "red", main = "Global Active Power", 
+     xlab = "Global Active Power (kilowatts)")
+#Dev Off
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,41 @@
+#Exploratory Data Analysis Assignment 1 - Coursera
+#Plot 3
+#Allen Seol 
+
+#Packages Used - dplyr
+#install.packages("dplyr")
+#library("dplyr")
+
+#Loading Data
+power <- read.table("household_power_consumption.txt", sep = ";", header = TRUE)
+
+#Converting to TBL Dataframe (working copy)
+power2 <- tbl_df(power)
+
+#Converting Time/Date into correct classes
+power2 <- mutate(power2, Date = as.Date(power2$Date, format ="%d/%m/%Y"))
+power2 <- mutate(power2, Time = paste(power2$Date,power2$Time))
+power2 <- mutate(power2, Time = as.POSIXct(power2$Time,format = "%Y-%m-%d %H:%M:%S"))
+
+#Subsetting Into 2007-02-01 and 2007-02-02 Dates and combing into Power5
+power3 <- filter(power2, Date == as.Date("2007-02-01")) 
+power4 <- filter(power2, Date == as.Date("2007-02-02"))
+power5 <- bind_rows(power3,power4)
+
+#Converting Important Variables from factor to integer
+power5 <- mutate(power5, Global_active_power = as.numeric(as.character(power5$Global_active_power)))
+power5 <- mutate(power5, Sub_metering_1 = as.numeric(as.character(power5$Sub_metering_1)))
+power5 <- mutate(power5, Sub_metering_2 = as.numeric(as.character(power5$Sub_metering_2)))
+power5 <- mutate(power5, Sub_metering_3 = as.numeric(as.character(power5$Sub_metering_3)))
+
+#Exporting to Graphic Device
+png(file = "plot3.png", width = 480, height = 480)
+
+#plotting components with legend
+with(power5, plot(Time,Sub_metering_1,type = "l", ylab = "Energy sub metering"), type ="n")
+with(power5, lines(Time,Sub_metering_2,col ="red"))
+with(power5, lines(Time,Sub_metering_3,col="blue"))
+legend("topright",lty = c(1,1,1),col = c("black","red","blue"),legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+
+#dev off
+dev.off()
